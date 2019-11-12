b1d90effd90631d548bbdd02781e51015afa88be
Baikai <baikaiwang@Baikais-MacBook-Pro.local>
2019-11-12 15:47:57 -0500

    simple tokens

--- lex.py
+++ lex.py
@@ -1,1 +1,55 @@
+import ply.lex as lex
 
+# List of token names.   This is always required
+tokens = (
+   'INT',
+   'PLUS',
+   'MINUS',
+   'STAR',
+   'SLASH',
+)
+
+# Regular expression rules for simple tokens
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_STAR    = r'\*'
+t_SLASH   = r'/'
+
+
+# A regular expression rule with some action code
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+# Test it out
+data = '''
+3 + 4 * 10
+  + -20 *2
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok)
