e9a474343282382a6c017bcf155e2ae749ac29c2
quanze <manqz@mcmaster.ca>
2019-11-17 14:27:28 -0500

    commit

--- lex.py
+++ lex.py
@@ -1,5 +1,4 @@
 import ply.lex as lex
-
 # List of token names.   This is always required
 tokens = (
     'AND',
@@ -77,92 +76,30 @@ tokens = (
     'TRY',
     'TYPEOF',
     'VOID',
-    'WHILE LP'
+    'WHILE_LP'
 )
-# AND symbol &
-# ASSIG symbol =
-# BOOL keyword bool
-# BREAK keyword break
-# CATCH keyword catch
-# CHAR keyword char
-# CHAR LIT char literal
-# CLASS keyword class
-# CLASSNAME identier that has been previously dened or declared
-# as a name of a class
-# CLASSNAME DOT CLASSANAME followed by .
-# CLASSNAME LP CLASSANAME followed by (
-# COLON symbol :
-# COMMA symbol ,
-# CONST keyword const
-# CONTINUE keyword continue
-# DOT symbol .
-# ELSE keyword else
-# EQ symbol ==
-# EXTENDS keyword extends
-# FALSE keyword false
-# FLOAT keyword float
-# FLOAT LIT float literal
-# FOR LP keyword for followed by (
-# GE symbol >=
-# GOTO keyword goto
-# GTGT symbol >>
-# ID identier
-# ID LP ID followed by (
-# ID COLON ID followed by :
-# IDOF keyword ideof
-# IF LP keyword if followed by (
-# INT keyword int
-# INT LIT int literal
-# LB symbol {
-# LE symbol <=
-# LP symbol (
-# LS symbol [
-# LT symbol <
-# LTLT symbol <<
-# MAIN LP keyword main followed by (
-# MINUS symbol -
-# MINUSMINUS symbol --
-# MOD symbol %
-# NEQ symbol !=
-# NEW keyword new
-# NOREF keyword noref
-# NOT symbol !
-# OR symbol |
-# PARENT DOT PARENT followed by .
-# PARENT LP PARENT followed by (
-# PASSIG symbol @=
-# PEEKNOTELSE the lookahead token indicating that the next token is not ELSE
-# PERM keyword permanent
-# PLUS symbol +
-# PLUSPLUS symbol ++
-# PRETURN keyword @return
-# PRIVATE keyword private
-# PUBLIC keyword public
-# RB symbol }
-# RETURN keyword return
-# RP symbol )
-# RS symbol ]
-# SEPICOL symbol ;
-# SHARED keyword shared
-# SIZEOF keyword sizeof
-# Appendix B 387
-# SLASH symbol /
-# STAR symbol *
-# STRING keyword string
-# STRING LIT string literal
-# TERMINATE keyword terminate
-# THROW keyword throw
-# TRUE keyword true
-# TRY keyword try
-# TYPEOF keyword typeof
-# VOID keyword void
-# WHILE LP keyword while followed by (
+
 
 # Regular expression rules for simple tokens
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_STAR    = r'\*'
 t_SLASH   = r'/'
+t_AND     = r'&'
+t_ASSIGN  = r'='
+t_BOOL    = r'bool'
+t_BREAK   = r'break'
+t_CATCH   = r'catch'
+t_CHAR    = r'char'
+#t_CHAR_LIT=  r'\"[a-zA-Z]{1}\"' Made with action code 
+t_CLASS   = r'class'
+t_CLASSNAME= r'[A-Z][a-zA-Z_0-9]*' 
+t_CLASSNAME_DOT = r'\.'
+t_CLASSNAME_LP  = r'\('
+t_COLON   = r':'
+t_COMMA   = r','
+t_CONST   = r'const'
+t_CONTINUE= r'continue'
 
 
 # A regular expression rule with some action code
@@ -170,6 +107,12 @@ def t_INT(t):
     r'\d+'
     t.value = int(t.value)
     return t
+
+
+def t_CHAR_LIT(t):
+    r'\"[a-zA-Z]{1}\"'
+    t.value = t.value[1]
+    return t 
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -189,8 +132,10 @@ lexer = lex.lex()
 
 # Test it out
 data = '''
-3 + 4 * 10
-  + -20 *2
+bool &
+char p = "x"
+
+class Boxes()
 '''
 
 # Give the lexer some input
